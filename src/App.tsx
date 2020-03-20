4fc8471d9617935d3997041ee78c8c558225627e
Lenz Weber <lenz.weber@mayflower.de>
2020-03-20 16:31:35 +0100

    remove redux implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 import React from "react";
 import { Board } from "./Board";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "./logic/state";
 import { Piece, Color } from "./logic/board";
-import {
-  Phase,
-  game,
-  selectHighlightedFields as selectReduxHighlightedFields
-} from "./logic/game";
-import {
-  machine,
-  selectHighlightedFields as selectXStateHighlightedFields
-} from "./logic/xStateGame";
+import { machine, selectHighlightedFields } from "./logic/xStateGame";
 import { useMachine } from "@xstate/react";
 
-const actions = game.actions;
-
 const App = () => {
-  const reduxGame = useSelector((state: RootState) => state);
-  const reduxDispatch = useDispatch();
-  const reduxHighlightedFields = useSelector(selectReduxHighlightedFields);
-
-  const [machineGame, machineDispatch] = useMachine(machine, {
+  const [machineState, machineDispatch] = useMachine(machine, {
     devTools: true
   });
-  const xMachineHighlightedFields = selectXStateHighlightedFields(machineGame);
+  const { context: game, value: phase } = machineState;
 
-  const game = machineGame.context;
+  return (
+    <>
+      <p>Current player: {game.currentPlayer}</p>
+      <p>Current Phase: {phase}</p>
+      <Board
+        board={game.board}
+        pieces={game.pieces}
+        higlighted={selectHighlightedFields(machineState)}
+        fieldClicked={fieldClicked}
+        pieceClicked={pieceClicked}
+      />
+    </>
+  );
 
   function pieceClicked(piece: Piece) {
     switch (piece.color) {
@@ -45,47 +41,10 @@ const App = () => {
           targetPiece: piece.id
         });
     }
-
-    // Piece
-    if (reduxGame.phase === Phase.selectPiece) {
-      reduxDispatch(actions.pieceSelected(piece.id));
-    }
-    if (reduxGame.phase === Phase.selectTarget) {
-      reduxDispatch(actions.targetSelected({ piece: piece.id }));
-    }
   }
   function fieldClicked(field: number) {
     machineDispatch({ type: "SELECTED_FIELD", targetField: field });
-    // Field
-    if (reduxGame.phase === Phase.selectBlackTarget) {
-      reduxDispatch(actions.blackTargetSelected(field));
-    }
-    if (reduxGame.phase === Phase.selectGrayTarget) {
-      reduxDispatch(actions.grayTargetSelected(field));
-    }
-    if (reduxGame.phase === Phase.selectTarget) {
-      reduxDispatch(actions.targetSelected({ field }));
-    }
   }
-
-  const highlightedFields = xMachineHighlightedFields;
-
-  //const phase = Phase[phasereduxGame.phase];
-  const phase = machineGame.value;
-
-  return (
-    <>
-      <p>Current player: {game.currentPlayer}</p>
-      <p>Current Phase: {phase}</p>
-      <Board
-        board={game.board}
-        pieces={game.pieces}
-        higlighted={highlightedFields}
-        fieldClicked={fieldClicked}
-        pieceClicked={pieceClicked}
-      />
-    </>
-  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -2,19 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
-import { Provider } from "react-redux";
-import { store } from "./logic/state";
-import { ToastContainer } from "react-toastify";
 
-import "react-toastify/dist/ReactToastify.css";
-
-ReactDOM.render(
-  <Provider store={store}>
-    <App />
-    <ToastContainer position="bottom-left" />
-  </Provider>,
-  document.getElementById("root")
-);
+ReactDOM.render(<App />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
